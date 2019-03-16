4842c438eb0abb8e7f2941e8f45dc35e7ea9af33
Dr-WaSaBi <russellriker@gmail.com>
2019-03-15 23:07:10 -0400

    added comment to javaDoc

--- src/main/java/org/usfirst/frc5534/Onabot/OI.java
+++ src/main/java/org/usfirst/frc5534/Onabot/OI.java
@@ -21,6 +21,7 @@ import org.usfirst.frc5534.Onabot.BobController;
 /**
  * This class is the glue that binds the controls on the physical operator
  * interface to the commands and command groups that allow control of the robot.
+ * These can be on a joystick or controller, on as buttons on the drivers station.
  */
 public class OI {
     //// CREATING BUTTONS
