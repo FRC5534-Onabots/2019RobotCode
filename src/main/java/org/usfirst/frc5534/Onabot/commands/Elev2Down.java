ade6c5d62e560e6357cb6d1c26a08e6bdb1a9f43
Dr-WaSaBi <russellriker@gmail.com>
2019-03-15 23:15:51 -0400

    Pass in a speed from the Operator JS right Y to up and down

--- src/main/java/org/usfirst/frc5534/Onabot/commands/Elev2Down.java
+++ src/main/java/org/usfirst/frc5534/Onabot/commands/Elev2Down.java
@@ -44,7 +44,7 @@ public class Elev2Down extends Command {
     // Called repeatedly when this Command is scheduled to run
     @Override
     protected void execute() {
-        Robot.elevator2.down();
+        Robot.elevator2.down(Robot.oi.getOperatorRightJoyStickY());
     }
 
     // Make this return true when this Command no longer needs to run execute()
--- src/main/java/org/usfirst/frc5534/Onabot/commands/Elev2Up.java
+++ src/main/java/org/usfirst/frc5534/Onabot/commands/Elev2Up.java
@@ -44,7 +44,7 @@ public class Elev2Up extends Command {
     // Called repeatedly when this Command is scheduled to run
     @Override
     protected void execute() {
-        Robot.elevator2.up();
+        Robot.elevator2.up(Robot.oi.getOperatorRightJoyStickY());
     }
 
     // Make this return true when this Command no longer needs to run execute()
